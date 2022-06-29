13e1159551c764a82fde8a29395d4783587a9635
zumma-seum <itisalwaysgood@gmail.com>
2022-06-29 10:44:06 +0900

    learn middleware2

--- src/server.js
+++ src/server.js
@@ -3,16 +3,29 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
     console.log(`${req.method} : ${req.url}`);
     next();
 };
+const privateMiddleware = (req,res,next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("allowed, you may continue.")
+    next();
+}
 const handleHome = (req, res) => {
     return res.send("I love middlewares");
 };
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge");
+}
 
-app.use(gossipMiddleware);
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/",handleHome);
+app.get("/", handleProtected);
 
 const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
