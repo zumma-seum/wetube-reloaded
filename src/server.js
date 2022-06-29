f1e3b288d005ad2a5d299e5009065622a69b96cb
zumma-seum <itisalwaysgood@gmail.com>
2022-06-29 10:21:55 +0900

    middleware
    example middleware

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const gossipMiddleware = (req, res, next) => {
+    console.log(`${req.method} : ${req.url}`);
+    next();
+};
+const handleHome = (req, res) => {
+    return res.send("I love middlewares");
+};
+
+app.get("/", gossipMiddleware,handleHome);
+
+const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
