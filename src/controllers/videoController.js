1f8ad67b2ede7aaf452993d3ea94176ce6cabb0d
zumma-seum <itisalwaysgood@gmail.com>
2022-07-04 10:33:25 +0900

    create pug file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-} 
+    return res.render("watch");
+};
 export const edit = (req, res) => {
     return res.send("Edit");
 };
